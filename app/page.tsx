d98d8bfabe4486efe35194faa289f38057fce57e
jrodgers <jrodgers@astate.edu>
2025-12-12 10:22:36 -0600

    Implement Switch

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,21 @@
 'use client'
 
+import { useState } from "react";
 import { toggleMonitors } from "./actions";
+import { Switch } from "@mui/material";
 
 export default function Home() {
 
+  const [checked, setChecked] = useState(true)
   const handleToggle = async ()=>{
+    setChecked(!checked)
     await toggleMonitors();
   }
   return (
-    <button onClick={handleToggle}>toggle</button>
+    <>
+    Toggle Monitors
+    <Switch checked={checked} onChange={handleToggle} />
+
+    </>
   );
 }
