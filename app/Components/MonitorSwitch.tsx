2722d34fbaec4a808c01f6dd20d64e08ea8abd84
jrodgers <jrodgers@astate.edu>
2025-12-15 09:46:25 -0600

    Fix double server action on switch

--- app/Components/MonitorSwitch.tsx
+++ app/Components/MonitorSwitch.tsx
@@ -85,7 +85,6 @@ export default function MonitorSwitch()
           
                 <Switch 
                     checked={checked} 
-                    onChange={handleToggle}
                 />
             </Box>
         </Box>
